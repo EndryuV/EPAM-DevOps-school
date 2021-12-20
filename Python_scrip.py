5a2326aa31f6b62b31fce17f6c5c0c139af7b250
EndryuV <92988788+EndryuV@users.noreply.github.com>
2021-12-20 22:05:20 +0300

    Update Python_scrip.py

--- Python_scrip.py
+++ Python_scrip.py
@@ -1,6 +1,4 @@
 import psycopg2
-
-#from prettytable import from_db_cursor
 from config import host, database, user, password
 
 try:
@@ -11,45 +9,54 @@ try:
         database=database,
     )
     cursor = conn.cursor()
-    cursor.execute('select magazines.name, article_types.type, author.author from magazines, article_types, author, articles where articles.magazines_id = magazines.id and articles.article_types_id = article_types.id and articles.author_id = author.id;')
-    #print(cursor.rowcount)
+    cursor.execute('select magazines.name, article_types.type, author.author from magazines, article_types, author, articles where articles.magazines_id = magazines.id and articles.article_types_id = article_types.id and articles.author_id = author.id;')    
     row = cursor.fetchone()
-    articles = []
-
+    info = []
     while row is not None:
-        articles.append(row)
-        print(row)
-        row = cursor.fetchone()
-    #print(articles)
-    conn.commit() 
+        info.append(row)        
+        row = cursor.fetchone()        
 finally:
     conn.close()
-contents = """<html>
-<head>
-        <meta charset="UTF-8">
-        <title>My table</title>
-</head>
-<style>table, th, td {border:1px solid black;}</style>
-<body>
-    <h2 align="center">Articles</h2>
-    <table style="width: 100%;"
-        <tr>
+contents = """<table class="table">
+	<thead>
+		<tr>
             <th>magazines_id</th>
             <th>article_type_id</th>
             <th>author_id</th>
-        </tr>"""
-
-contents2 = """</table>
-</body>
-</html>"""
-
-with open('index_.html', 'w') as news:
+        </tr>
+    </thead>
+    <tbody>
+    """
+contents2 = """</tbody>
+</table>
+<style type="text/css">
+.table {
+	width: 100%;
+	margin-bottom: 20px;
+	border: 15px solid #F2F8F8;
+	border-top: 5px solid #F2F8F8;
+	border-collapse: collapse; 
+}
+.table th {
+	font-weight: bold;
+	padding: 5px;
+	background: #F2F8F8;
+	border: none;
+	border-bottom: 5px solid #F2F8F8;
+}
+.table td {
+	padding: 5px;
+	border: none;
+	border-bottom: 5px solid #F2F8F8;
+}
+</style>"""
+with open('/usr/share/nginx/html/index2.html', 'w') as news:
     news.write(contents)
-    for tup in articles:
+    for tup in info:
         news.write("<tr>")
         news.write("\n")
         for x in tup:
-            text = "<th><center>"+str(x)+"</center></th>"+"\n"
+            text = "<td><center>"+str(x)+"</center></td>"+"\n"
             news.write(text)
         news.write("</tr>")
         news.write("\n")
