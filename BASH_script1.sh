272af19a934e77a1a2b0ff8fd3c20c99cde49a53
EndryuV <92988788+EndryuV@users.noreply.github.com>
2021-12-20 22:08:44 +0300

    Update BASH_script1.sh

--- BASH_script1.sh
+++ BASH_script1.sh
@@ -2,5 +2,5 @@
 psql -U postgres -p 5432  db  <<< 'select * FROM public.articles' > /local/files/file$(date +%s)
 a=$(find /local/files -type f| wc -l)
 if [ "$a" -gt 3 ]
-then zip archive /local/files/ -9 -r | mv archive.zip /local/backups/archive$(date +%s) | rm -f /local/files/*
+then tar -cvf /local/backups/archive$(date +%s) /local/files/
 fi
